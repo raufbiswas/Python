b4afb11dcbef5257d5f4d5a918da4383ae22b16f
B M Rauf <bmrauf@outlook.com>
2023-11-21 13:32:13 +0600

    Update Auto Send Messages.py

--- Auto Send Messages.py
+++ Auto Send Messages.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 n = 0
 while n<30: #as much time you want to send message
-    pyautogui.sleep(10)
+    pyautogui.sleep(4) #it will send a message every 4 seconds
     pyautogui.typewrite("Amake ekta Macbook Pro M2 Max 1TB storage er laptop kine diba?:)")
     pyautogui.press('enter')
     n+=1
